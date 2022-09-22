dcf8258ea3b8e5a92b42c33394bfaad7898f71e9
Yanni Leondaridis <yleondaridis@mitre.org>
2022-09-22 14:42:56 -0400

    Please work

--- config/run.sh
+++ config/run.sh
@@ -6,10 +6,10 @@ msg() {
 msg "Will run this Node service as $RUN_MODE mode..."
 
 if [[ "$RUN_MODE" == "development" ]]; then
-    yarn run start --port 80 --host 0.0.0.0 &
+    yarn run start --port 8080 --host 0.0.0.0 &
 elif [[ "$RUN_MODE" == "production" ]]; then
    msg "Build current sources..."
-   yarn run serve --build --port 80 --host 0.0.0.0 &
+   yarn run serve --build --port 8080 --host 0.0.0.0 &
 elif [[ "$RUN_MODE" != "production" ]] && [[ "$RUN_MODE" != "development" ]]; then
    msg "This "$RUN_MODE" mode is unknown as a default Node.js service mode. You should do know what you do."
    yarn run "$RUN_MODE" &
